65b67d6cbc933bf7e46023e4439718d9b7ab5e20
christoper <mschristoper2@gmail.com>
2021-09-16 22:28:20 +0800

    add dashboard

--- LoginProject/accounts/views.py
+++ LoginProject/accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
+from django.db.models import Sum
 
 # Create your views here.
 def home(request):
@@ -16,7 +17,18 @@ def signup(request):
     return render(request,'accounts/signup.html')
 
 def dashboard(request):
-    return render(request,'accounts/dashboard.html')
+    slp = SLP.objects.all()
+    player = User.objects.all()
+
+    total_win = slp.filter(Mode='Win').count()
+    total_lose = slp.filter(Mode='lose').count()
+    totalLoseWin = total_lose+total_win
+    WinRate = total_win/totalLoseWin * 100.0
+
+    TotalSLP = SLP.objects.aggregate(Sum('slp'))['slp__sum']
+    context = {'player':player, 'slp':slp,'WinRate':WinRate,'TotalSLP':TotalSLP}
+
+    return render(request,'accounts/dashboard.html',context)
 
 def sSLP(request):
     slp = SLP.objects.all()
